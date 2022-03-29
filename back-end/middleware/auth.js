1fde393d17f90c8a6ce25fdf4dd038bf39a647a7
P2-openclassroom <jonh_06@live.fr>
2022-03-29 16:06:15 +0200

    again change

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -12,16 +12,17 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];  //retourner un tableau bearer  1 element , token 2 element (que l'on recupere)
     //decoder le token
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY ); // function pour verifier le token et le token deja crée dans le  .env
+    const admin = decodedToken.admin //decoder le token de l'admin
     //une fois decoder il devient un objet classique pour le recuperer
     const userId = decodedToken.userId; // recuperer le userid qui est a l'interieure
     //attribuer userid a la requete , objet auth a la req
-    req.auth = { userId } ; //racourcie pour attribuer un valeur a une clée du mm nom d'un objet,extensible , ajouter des element a l'avenir
+    req.auth = { userId , admin} ; //racourcie pour attribuer un valeur a une clée du mm nom d'un objet,extensible , ajouter des element a l'avenir
     // verifier que la requete correspond a celle du token
     // si userid dans le corp de la req et que celuici et different de userId
     if (req.body.userId && req.body.userId !== userId)  {
         throw 'User iD non valable !'; //pour renvoyer l'erreur
     } else { //si tout vas bien
-        next(); // on pass la req au middleware suivant
+        next(); // on pass la req au middleware suivant (si auth est correct, pass au mid suivant ex multer)
     }
     } catch (error) {
         res.status(401).json({ error: error | 'Requete non authentifiée !' })  // '|' signifier ou (sinon) sur un autre erreur le msg
