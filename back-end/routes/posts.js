444f36938caeb9c590a4e646fa18458892f40028
P2-openclassroom <jonh_06@live.fr>
2022-04-01 13:32:34 +0200

    again

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -1,18 +1,18 @@
 //Imports
 const express = require('express');
 const router = express.Router();
-//const postControler = require('../controllers/posts');
-//const auth = require('../middleware/auth');
+const postController = require('../controllers/posts');
+const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 const limiter = require('../middleware/rate-limit'); //importe le fichier (limiter les requete avant auth)
 
 //Routage (crée ,lire , modifier ,supprimer un post)
-router.post("/", limiter.modify, auth, multer, postControler.create);
-router.get("/", limiter.get, auth,  postControler.allPosts);
-router.get("/:id", limiter.get, auth,  postControler.onePosts);
-router.put("/:id", limiter.modify, auth, multer, postControler.update)
-router.delete("/:id", postControler.delete)
+router.post("/", limiter.modify, auth, multer, postController.create);
+router.get("/", limiter.get, auth,  postController.allPosts);
+router.get("/:id", limiter.get, auth,  postController.onePosts);
+router.put("/:id", limiter.modify, auth, multer, postController.update)
+router.delete("/:id", postController.delete)
 //like un post
-router.post('/:id/like', limiter.modify, auth, postControllers.likePost);
+router.post('/:id/like', limiter.modify, auth, postController.likePost);
 
 module.exports = router;
