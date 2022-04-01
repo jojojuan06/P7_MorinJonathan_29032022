89f33b57979ce24c7583b0dca4fa609c374161cc
P2-openclassroom <jonh_06@live.fr>
2022-04-01 19:23:03 +0200

    modification fichier

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -2,8 +2,8 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+const Sequelize = require('sequelize'); //transcrire le javascript en mysql
+const basename = path.basename(__filename); //chercher le nom de mes model
 const env = process.env.NODE_ENV || 'development';
 //const config = require(__dirname + '/../config/config.js')[env];
 require('dotenv').config();// proteger les données .env 
