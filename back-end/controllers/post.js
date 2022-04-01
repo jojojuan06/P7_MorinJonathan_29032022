86050994c497bffa9c6117fc082701a705784318
P2-openclassroom <jonh_06@live.fr>
2022-04-01 12:24:53 +0200

    create post controller

--- back-end/controllers/post.js
+++ back-end/controllers/post.js
@@ -1,3 +1,44 @@
-// POST-- LIKE / DISLIKE UNE SAUCE
-//User.findAll();
-const { User, like , Post } = require('../models')
+// CREATE READ UPDATE DELTE POSTS AND LIKE 
+
+//importer acces au systeme de fichier  (action sur une fichier ou dossier)
+const fs = require('fs');
+
+//valide et nettoie uniquement les chaînes (validation de l'email)
+const validator = require('validator'); 
+const db = require('../models/index')
+
+
+
+
+
+
+//Creation d'un post POST
+
+
+
+//-------------
+
+//recuperer un post GET
+
+
+
+//-------------
+
+//recuperer tous les post GET ALL
+
+
+
+//-------------
+
+//mettre a jour un post PUT
+
+
+
+//-------------
+
+//supprimer un post DELETE
+
+
+
+//-------------
+
--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -7,12 +7,12 @@ const multer = require('../middleware/multer-config');
 const limiter = require('../middleware/rate-limit'); //importe le fichier (limiter les requete avant auth)
 
 //Routage (crée ,lire , modifier ,supprimer un post)
-// router.post("/", limiter.modify, auth, multer, postControler.create);
-// router.get("/", limiter.get, auth,  postControler.allPosts);
-// router.get("/:id", limiter.get, auth,  postControler.onePosts);
-// router.put("/:id", limiter.modify, auth, multer, postControler.update)
-// router.delete("/:id", postControler.delete)
+router.post("/", limiter.modify, auth, multer, postControler.create);
+router.get("/", limiter.get, auth,  postControler.allPosts);
+router.get("/:id", limiter.get, auth,  postControler.onePosts);
+router.put("/:id", limiter.modify, auth, multer, postControler.update)
+router.delete("/:id", postControler.delete)
 //like un post
-//router.post('/:id/like', limiter.modify, auth, postControllers.likeSauce);
+router.post('/:id/like', limiter.modify, auth, postControllers.likePost);
 
 module.exports = router;
