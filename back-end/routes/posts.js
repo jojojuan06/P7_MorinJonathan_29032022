eb5792b78c3e3f9f133fee32039006ef61aa0dd8
P2-openclassroom <jonh_06@live.fr>
2022-04-02 12:23:00 +0200

    change route delete auth

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -10,8 +10,8 @@ const limiter = require('../middleware/rate-limit'); //importe le fichier (limit
 router.post("/", limiter.modify, auth, multer, postController.createPost);//recupere un post
 router.get("/", limiter.get, auth,  postController.getAllPost); //recupere tout les posts
 router.get("/:id", limiter.get, auth,  postController.getOnePost);
-router.put("/:id", limiter.modify, auth, multer, postController.updatePost)
-router.delete("/:id", postController.deletePost)
+router.put("/:id", limiter.modify, auth, multer, postController.updatePost);
+router.delete("/:id", auth ,postController.deletePost);
 //like un post
 router.post('/:id/like', limiter.modify, auth, postController.likePost);
 
