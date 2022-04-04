59a835cecaa668c6b2e6256c0a1f11d66bfc5ce9
P2-openclassroom <jonh_06@live.fr>
2022-04-04 18:30:24 +0200

    comment controller

--- back-end/controllers/comment.js
+++ back-end/controllers/comment.js
@@ -85,7 +85,7 @@ exports.deleteComment = (req, res, next) => {
     //recuperer tous les commentaire GET ALL
     exports.getAllComment = (req, res, next) => {    
         //création des objet-----------
-        Comment.findAll() //{include:User}
+        Comment.findAll({include:User}) //recuperer la table user egalement
             .then(comments => res.status(200).json(comments)) // retourne la response 200 pour ok pour la methode http , revoi le tableaux des users recu
             .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` })); 
         }
