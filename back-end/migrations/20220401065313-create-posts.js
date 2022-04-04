5de406dd97effc0d0b2fdf9c7f1bf02d44d9ab79
P2-openclassroom <jonh_06@live.fr>
2022-04-04 08:53:03 +0200

    change again

--- back-end/migrations/20220401065313-create-posts.js
+++ back-end/migrations/20220401065313-create-posts.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userId: {
+      UserId: {
         allowNull: false,
         type: Sequelize.INTEGER,
         //reference au model crée users (id)
--- back-end/models/post.js
+++ back-end/models/post.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Post.init({
-    userId: {
+    UserId: {
       allowNull: false,
       type: DataTypes.INTEGER
     },
