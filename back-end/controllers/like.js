2c30f3efc99a1c7d9b75eda78757d40395905e68
P2-openclassroom <jonh_06@live.fr>
2022-04-05 15:45:58 +0200

    change

--- back-end/controllers/like.js
+++ back-end/controllers/like.js
@@ -33,31 +33,31 @@ exports.createLike = (req, res, next) => {
     }    
 }
 
-//mettre a jour d'un Commentaire PUT
+//mettre a jour d'un Like PUT
 exports.updateLike = (req, res, next) => {//exporter une function createuser / contenue de la route post / creation dun post
     Like.findOne({ where:{ id: req.params.id,}})
     .then(like => { // si l'utilisateur et admin il peut modif les utili ou juste l'util modif sont profil
     if (like.userId === req.auth.userId ||  req.auth.admin == true ) {
             let newLike = Object.assign(like,req.body); // remplace le post par le new post (objet,permet d'envoyer des champ vide(recupere un champ)) 
             newLike.save() //sauvegarde le nouveau post
-            .then(() => res.status(200).json({ message: 'Commentaire modifié !'}))// retourne la response 200 pour ok pour la methode http , renvoi objet modifier
+            .then(() => res.status(200).json({ message: 'Like modifié !'}))// retourne la response 200 pour ok pour la methode http , renvoi objet modifier
             .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` }));    
             } else {
-            res.status(403).json({ message: `vous n'etes pas autoriser a modifiée ce commentaire` });  
+            res.status(403).json({ message: `vous n'etes pas autoriser a modifiée ce like` });  
         }
     })
     .catch(error => res.status(404).json({ message: `nous faisons face a cette: ${error}` })); 
 };
 //-------------
 
-//supprimer un Commentaire DELETE
+//supprimer un Like DELETE
 exports.deleteLike = (req, res, next) => {
     // allez le chercher et avoir l'url de l'image pour la supprimer (cherche le produit)
-    Comment.findOne({ id: req.params.id })
+    Like.findOne({ id: req.params.id })
     //trouver id a celui qui est dans les parametres de la req ,recupere un post (produit) dans le callback (function de rapelle)
     .then((like) => {// recupere le post dans la base
-            if (!like) { // si le commentaire n'existe pas
-                return res.status(404).json({ message: "Le commentaire n'existe pas !"})
+            if (!like) { // si le like n'existe pas
+                return res.status(404).json({ message: "Le  n'existe pas !"})
             }
             // verifier que seulement la personne qui detient l'objet peu le supprimer
             if (like.userId !== req.auth.userId) { //different de req.auth
@@ -67,24 +67,24 @@ exports.deleteLike = (req, res, next) => {
             }
             //recuperer l'id des paramettre de route ,si oui on effectue la suppression
             like.destroy() // supprime le post crée   
-            .then(() => res.status(200).json({message: 'Commentaire supprimer !'})) // retourne la response 200 pour ok pour la methode http , renvoi objet modifier
+            .then(() => res.status(200).json({message: 'Like supprimer !'})) // retourne la response 200 pour ok pour la methode http , renvoi objet modifier
             .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` })); // capture l'erreur et renvoi un message erreur (egale error: error)    
         })
         .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` }));
     };
     //-------------
     
-    //recuperer un commentaire GET
+    //recuperer un like GET
     exports.getOneLike = (req, res, next) => { 
         let id = req.params.id // avoir acces  dans l'objet req.pams.id
         Like.findOne( { where:{id: id},//trouver un objet avec where , on pass l'objet en conparaison id  egal le parm de req id
         }) 
         .then(like => res.status(200).json(like)) // retourne la response 200 pour ok pour la methode http , renvoi l'objet (un objet)si il existe dans la Bd
-        .catch(error => res.status(404).json({ message: `Commentaire non trouvé: ${error}` }));
+        .catch(error => res.status(404).json({ message: `Like non trouvé: ${error}` }));
     }
     //-------------
     
-    //recuperer tous les commentaire GET ALL
+    //recuperer tous les like GET ALL
     exports.getAllLike = (req, res, next) => {    
         //création des objet-----------
         Like.findAll({include:User}) //recuperer la table user egalement
