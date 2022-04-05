7423bb515b088127a91e9cbc9d591294b638c7a5
P2-openclassroom <jonh_06@live.fr>
2022-04-05 18:37:34 +0200

    change comment controller

--- back-end/controllers/comment.js
+++ back-end/controllers/comment.js
@@ -36,7 +36,7 @@ exports.createComment = (req, res, next) => { //function de callback
 exports.updateComment = (req, res, next) => {//exporter une function createuser / contenue de la route post / creation dun post
     Comment.findOne({ where:{ id: req.params.id,}})
     .then(comment => { // si l'utilisateur et admin il peut modif les utili ou juste l'util modif sont profil
-    if (comment.userId === req.auth.userId ||  req.auth.admin == true ) {
+    if (comment.userId === req.auth.userId ||  req.auth.admin == true) {
             let newComment = Object.assign(comment,req.body); // remplace le post par le new post (objet,permet d'envoyer des champ vide(recupere un champ)) 
             newComment.save() //sauvegarde le nouveau post
             .then(() => res.status(200).json({ message: 'Commentaire modifié !'}))// retourne la response 200 pour ok pour la methode http , renvoi objet modifier
@@ -53,21 +53,23 @@ exports.updateComment = (req, res, next) => {//exporter une function createuser 
 exports.deleteComment = (req, res, next) => {
     // allez le chercher et avoir l'url de l'image pour la supprimer (cherche le produit)
     Comment.findOne({ id: req.params.id })
-    //trouver id a celui qui est dans les parametres de la req ,recupere un post (produit) dans le callback (function de rapelle)
-    .then((comment) => {// recupere le post dans la base
-            if (!comment) { // si le commentaire n'existe pas
+    //trouver id a celui qui est dans les parametres de la req
+    // recupere le post dans la base
+    .then((comment) => { 
+            // si le commentaire n'existe pas
+            if (!comment) { 
                 return res.status(404).json({ message: "Le commentaire n'existe pas !"})
             }
-            // verifier que seulement la personne qui detient l'objet peu le supprimer
-            if (comment.userId !== req.auth.userId) { //different de req.auth
-                return res.status(401).json({ //probleme authentification ,on verifier qu'il appartient bien  a la personne qui effectuer la req
-                    error: new Error('Requete non autorisé !')
-                });   
+            // verifier que seulement la personne qui detient l'objet peu le supprimer ou admin
+            if (comment.userId == req.auth.userId ||  req.auth.admin == true) { //different de req.auth
+                //recuperer l'id des paramettre de route ,si oui on effectue la suppression
+                comment.destroy() // supprime le commentaire   
+                .then(() => res.status(200).json({message: 'Commentaire supprimer !'})) // retourne la response 200 pour ok pour la methode http , renvoi objet modifier
+                .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` })); // capture l'erreur et renvoi un message erreur (egale error: error)    
+            } else {
+            // sinon 403 utilisateur non autorisation
+            return res.status(403).json({ message: 'utilisateur non autorisé !'});   
             }
-            //recuperer l'id des paramettre de route ,si oui on effectue la suppression
-            comment.destroy() // supprime le post crée   
-            .then(() => res.status(200).json({message: 'Commentaire supprimer !'})) // retourne la response 200 pour ok pour la methode http , renvoi objet modifier
-            .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` })); // capture l'erreur et renvoi un message erreur (egale error: error)    
         })
         .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` }));
     };
@@ -76,7 +78,8 @@ exports.deleteComment = (req, res, next) => {
     //recuperer un commentaire GET
     exports.getOneComment = (req, res, next) => { 
         let id = req.params.id // avoir acces  dans l'objet req.pams.id
-        Comment.findOne( { where:{id: id},//trouver un objet avec where , on pass l'objet en conparaison id  egal le parm de req id
+        //trouver un objet avec where , on pass l'objet en conparaison id  egal le parm de req id
+        Comment.findOne( { where:{id: id},
         }) 
         .then(comment => res.status(200).json(comment)) // retourne la response 200 pour ok pour la methode http , renvoi l'objet (un objet)si il existe dans la Bd
         .catch(error => res.status(404).json({ message: `Commentaire non trouvé: ${error}` }));
@@ -85,9 +88,10 @@ exports.deleteComment = (req, res, next) => {
     
     //recuperer tous les commentaire GET ALL
     exports.getAllComment = (req, res, next) => {    
-        //création des objet-----------
-        Comment.findAll({include:User}) //recuperer la table user egalement
-            .then(comments => res.status(200).json(comments)) // retourne la response 200 pour ok pour la methode http , revoi le tableaux des users recu
+        //recuperer la table user egalement
+        Comment.findAll({include:User}) 
+            // retourne la response 200 pour ok pour la methode http , revoi le tableaux des users recu
+            .then(comments => res.status(200).json(comments)) 
             .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` })); 
         }
     //-------------
