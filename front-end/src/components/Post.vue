47ad5aa3f3ac99a2e77da42b8037afcc0eaed093
P2-openclassroom <jonh_06@live.fr>
2022-04-09 20:46:03 +0200

    change again

--- front-end/src/components/Post.vue
+++ front-end/src/components/Post.vue
@@ -8,30 +8,16 @@
 
 <script>
 export default {
-    name: 'Post',
-    data(){
-        return {
-            title:"toto",
-            content:"titi"
-        }
-    },
-    created(){
-    this.getData();
-    },
-    methods : {
-    getData(){
-            const id =  this.$route.params.id;
-            fetch(`http://localhost:3000/api/post/${id}`,{
-                headers: { "Content-Type": "application/json" , 
-                "Authorization":"Bearer 123"}
-            })
-            .then((response) => response.json())
-            .then((post) => {  
-                this.title = post.title;
-                this.content = post.content;
-        })
-        .catch((err) => console.log('Erreur : ' + err)); /*attrape les erreur*/
+        name: 'Post',
+        props: {    
+        title: {
+            type:String,
+            default :'My title',
+        },
+        content: {
+            type:String,
+            default :'My content',
+        },
     }
-        }  
 }
 </script>
--- front-end/src/views/PostView.vue
+++ front-end/src/views/PostView.vue
@@ -14,5 +14,30 @@ export default {
   components: {
     Post
   }
-}
+    ,
+    data(){
+        return {
+            title:"toto",
+            content:"titi"
+        }
+    },
+    created(){
+    this.getData();
+    },
+    methods : {
+    getData(){
+            const id =  this.$route.params.id;
+            fetch(`http://localhost:3000/api/post/${id}`,{
+                headers: { "Content-Type": "application/json" , 
+                "Authorization":"Bearer 123"}
+            })
+            .then((response) => response.json())
+            .then((post) => {  
+                this.title = post.title;
+                this.content = post.content;
+        })
+        .catch((err) => console.log('Erreur : ' + err)); /*attrape les erreur*/
+    }
+        }
+}  
 </script>
