66c14c15aba5053b8ca508bfb7b81924ff3f3eb3
P2-openclassroom <jonh_06@live.fr>
2022-04-11 13:00:04 +0200

    access app change

--- back-end/app.js
+++ back-end/app.js
@@ -16,13 +16,19 @@ const likeRoutes = require('./routes/like.js');
 // appelle de la methode express (une function) permet de crée une application expresse
 const app = express();
 
+// utilisation du module----------
+//acces au corp de la requete (body) et met a disposition dans le corp (acces) sur objet req (body)
+app.use(express.json());// intercepte toute les requetes qui on content type json (format) 
+app.use(xss());  //nettoie les données d'entrée de l'utilisateur
 
 //--------Acces serveurs (CORS)
-
+app.use(helmet()); // helmet
 //reponse par default et donc retourn bien notre application on recoit l'objet request response et next pour passez middleware suivant
 app.use((req, res, next) => { 
     // qui peut acceder a l'api '*' (signifie all tous le monde  a acces au serveur origin)
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // autorise les resource de l'api pour utiliser sur les autre sites
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin'); 
      //autorise certaine header (en tete)sur l'objet requete
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
      //autorise certaine Requete
@@ -30,12 +36,7 @@ app.use((req, res, next) => {
     next();
 });
 
-// utilisation du module----------
-//acces au corp de la requete (body) et met a disposition dans le corp (acces) sur objet req (body)
-app.use(express.json());// intercepte toute les requetes qui on content type json (format) 
-app.use(xss());  //nettoie les données d'entrée de l'utilisateur
 
-app.use(helmet()); // helmet
 
 //Gestion des routes principales
 // Permet d'accéder aux routes pour les utilisateurs, les publications et les images
