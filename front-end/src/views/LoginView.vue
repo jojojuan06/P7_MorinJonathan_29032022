1745c1d2c64fb10f867b382c2d98bbea26f19cec
P2-openclassroom <jonh_06@live.fr>
2022-04-13 10:39:11 +0200

    change css

--- front-end/src/views/LoginView.vue
+++ front-end/src/views/LoginView.vue
@@ -8,29 +8,30 @@
             </v-card-title>
             <v-card-text v-if="mode == 'login'">
                 Tu as deja un compte ?
-                <v-btn  class="btn--first" v-on:click="switchToCreateAccount()">Crée un compte</v-btn>   <!--href="#" target="_blank" -->
+                <v-btn  class="btn--first" v-on:click="switchToCreateAccount">Crée un compte</v-btn>   <!--href="#" target="_blank" -->
         </v-card-text>
         <v-card-text v-else>
                 Tu n'as pas encore de compte ?
-                <v-btn  class="btn--first" v-on:click="switchToCreateLogin()">Se connecter</v-btn>   
+                <v-btn  class="btn--first" v-on:click="switchToLogin">Se connecter</v-btn>   
         </v-card-text>
         <v-form>
-            <v-text-field   v-model="form.email" type="text" label="E-mail" required></v-text-field>  
+            <v-text-field   v-bind:rules="emailRule" v-model="form.email" type="text" label="E-mail" required></v-text-field>  
         </v-form>
         <!-- afffiche seulement a la creation du compte -->    
-        <v-form  v-if="mode == 'createAccount'">
-            <v-text-field  v-model="form.name" type="text" label="Name" required></v-text-field>
-            <v-text-field v-model="form.firstname" type="text" label="Firstname" required></v-text-field>
+        <v-form  v-if="mode == 'create'">
+            <v-text-field v-bind:rules="nameRule"  v-model="form.name" type="text" label="Name" required></v-text-field>
+            <v-text-field v-bind:rules="firstnameRule" v-model="form.firstname" type="text" label="Firstname" required></v-text-field>
         </v-form>
         <v-form>   
-            <v-text-field  v-model="form.password" type="password" label="Password" required></v-text-field>
+            <v-text-field v-bind:rules="passwordRule" v-model="form.password" type="password" label="Password" required></v-text-field>
         </v-form>
         <v-form>
             <!-- si champ vide on disable le bouton validatedFieldss--> 
-            <v-btn  @click="login" :class="{'v-btn--disabled' : !validatedField}" v-if="mode == 'login'"  color="success" class="mr-4">
+            <v-btn  class="v-btn--disabled mr-4" v-if="mode == 'login'"  color="success">
                 Connexion
             </v-btn>
-            <v-btn :class="{'v-btn--disabled' : !validatedField}"  v-else color="success" class="mr-4" >
+            <!-- au clic appel a la methode createAccount--> 
+            <v-btn   v-on:click="createNewAccount" :class="{'v-btn--disabled' : !validatedField}"  v-else color="success" class="mr-4" >
                 Créer mon compte
             </v-btn>
         </v-form>
@@ -56,7 +57,7 @@ export default {
         validatedField: function() {
             //return false par default et true quand les  champ sont rempli
             let valid = false;
-            if (this.mode == 'createAccount') {
+            if (this.mode == 'create') { //createAccount
                 if (this.form.email != "" && this.form.firstname != "" && this.form.name != "" && this.form.password != "") {
                     valid = true;
                 } 
@@ -69,15 +70,15 @@ export default {
             } 
         },
     methods: { //function pour different etat
-        switchToCreateAccount: function() {
-            this.mode = 'createAccount';
+        switchToCreateAccount() {              // <------: function()
+            this.mode = 'create';
         },
-        switchToCreateLogin: function() {
+        switchToLogin() {                      // <------: function()
             this.mode = 'login';
         },
-        login() {
-            console.log(validatedField);
-        }
+        createNewAccount(){
+            console.log(this.email, this.name, this.firstname, this.password); 
+        } 
     },
 }
 </script>
