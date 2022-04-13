ddf8a4aed4d7bcf736ba6468ed84421a6dd01d22
P2-openclassroom <jonh_06@live.fr>
2022-04-13 17:41:58 +0200

    add state and mutation

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -7,10 +7,24 @@ import axios from '../axios';
 
 export default createStore({
   state: {
+    //data global (status vide)
+    status: '',  // contiendra le payload 
+    user: {
+      userId: -1,
+      token:'',
+    },
   },
   getters: {
   },
+  //mettre à jour et modifier nos données dans Vuex avec les mutations en paramettre le state et 2 payload
   mutations: {
+    //------> : function()
+    setStatus(state , status) {   
+        state.status = status;
+      },
+    logUser(state, user) {
+        state.user = user;
+    }   
   },
   actions: {  //similaire a la proprieter methods
     //recuperation du commit (invoquer une mutation avec  2params) nom a la creation du compte et 2e param le payload user info que l'on recupere
@@ -22,10 +36,12 @@ export default createStore({
         //requete Post enregistrer l'utilisateur
         axios.post('/auth/signup', userInfos) 
         .then(function (response) {
+        commit('setStatus' , 'created'); //type et payload
         //si tout dse pass bien
         resolve(response); //resolved (promesse résolue ) 
         })
         .catch(function (error) {
+          commit('setStatus' , 'error_create'); //type et payload
         //retourne une erreur
         reject(error); //rejected (rompue) : l'opération a échoué.
         });
@@ -33,17 +49,22 @@ export default createStore({
     },
     //recuperation du commit (invoquer une mutation avec  2params)
     loginAccount: ({commit}, userInfos) => {
+    //Pour invoquer un gestionnaire de mutation, vous devez appeler store.commitavec son type en un et Valider avec Payload en 2e argument 
+      commit('setStatus' , 'loading');
       //créeation d'un nouvelle promess
       //associer une action ultérieure à une promesse lorsque celle-ci devient acquittée 
       return new Promise((resolve, reject) => {
-        commit;
         //requete Post enregistrer l'utilisateur
         axios.post('/auth/login', userInfos) 
         .then(function (response) {
-          //si tout dse pass bien
+          commit('setStatus' , ''); //invoquer la mutation
+          // commit pour stocker notre user
+          commit('logUser', response.data) // deuxieme argument on recupere les data
+          //si tout se pass bien
           resolve(response); //resolved (promesse résolue ) 
         })
         .catch(function (error) {
+          commit('setStatus' , 'error_login'); //type et payload
           //retourne une erreur
           reject(error); //rejected (rompue) : l'opération a échoué.
         });
