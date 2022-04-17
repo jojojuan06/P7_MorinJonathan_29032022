1b726af019534e90513dd5ffac81312ac81acaf0
P2-openclassroom <jonh_06@live.fr>
2022-04-17 17:27:07 +0200

    change controller

--- back-end/controllers/like.js
+++ back-end/controllers/like.js
@@ -57,28 +57,35 @@ exports.deleteLike = (req, res, next) => {
     .then(post => {
         if (!post) { // si le post n'existe pas
             return res.status(404).json({ message: "Le post n'existe pas !"})
-        } //rechercher poste_id et user_id (de la bd)
-        post.decrement('likes') //enleve un like
-        post.save()
-        // allez le chercher et avoir l'url de l'image pour la supprimer (cherche le produit)
-        Like.findOne({ id: req.params.id })
-        //trouver id a celui qui est dans les parametres de la req ,recupere un post (produit) dans le callback (function de rapelle)
-        // recupere le post dans la base
-        .then((like) => {
-                if (like == 0) { // si le like n'existe pas
-                return res.status(404).json({ message: "Vous avez deja supprimer le like !"})
-            }
-            // verifier que seulement la personne qui detient l'objet peu le supprimer
-            if (like.userId !== req.auth.userId) { //different de req.auth
-                //probleme authentification ,on verifier qu'il appartient bien  a la personne qui effectuer la req
-                return res.status(401).json({ message: 'Requete non autorisé !'});   
-            }
-            //recuperer l'id des paramettre de route ,si oui on effectue la suppression
-            like.destroy() // supprime le post crée   
-            .then(() => res.status(200).json({message: 'Like supprimer !'})) // retourne la response 200 pour ok pour la methode http , renvoi objet modifier
-            .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` })); // capture l'erreur et renvoi un message erreur (egale error: error)    
-        })
-        .catch(error => res.status(500).json({ message: `nous faisons face a cette: ${error}` }));
+        }
+        if(post.likes == 0) {
+            console.log(post.likes);
+            return res.status(404).json({ message: "Veuillez liker le post avant de supprimer le like !"})
+        }
+        else {
+            post.decrement('likes') //enleve un like
+            post.save()
+            // allez le chercher et avoir l'url de l'image pour la supprimer (cherche le produit)
+            Like.findOne({ id: req.params.id })
+            //trouver id a celui qui est dans les parametres de la req ,recupere un post (produit) dans le callback (function de rapelle)
+            // recupere le post dans la base
+            .then((like) => {
+                if (!like) { // si le like n'existe pas
+                    return res.status(404).json({ message: "Le like n'existe pas !"})
+                }
+                // verifier que seulement la personne qui detient l'objet peu le supprimer
+                if (like.userId !== req.auth.userId) { //different de req.auth
+                    //probleme authentification ,on verifier qu'il appartient bien  a la personne qui effectuer la req
+                    return res.status(401).json({ message: 'Requete non autorisé !'});   
+                }
+                //recuperer l'id des paramettre de route ,si oui on effectue la suppression
+                like.destroy() // supprime le post crée   
+                .then(() => res.status(200).json({message: 'Like supprimer !'})) // retourne la response 200 pour ok pour la methode http , renvoi objet modifier
+                .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` })); // capture l'erreur et renvoi un message erreur (egale error: error)    
+            })
+            .catch(error => res.status(500).json({ message: `nous faisons face a cette: ${error}` }));    
+        } 
+        
     }).catch(error => res.status(500).json({ message: `nous faisons face a cette: ${error}` }));
 };
     //-------------
