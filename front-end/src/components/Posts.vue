d32002d415aa25e4a8c47ef4319ed32991acdc39
P2-openclassroom <jonh_06@live.fr>
2022-04-17 09:30:12 +0200

    add date create post

--- front-end/src/components/Posts.vue
+++ front-end/src/components/Posts.vue
@@ -5,6 +5,7 @@
             <!-- boucle for pour afficher la liste des posts , boucle sur chacun d'eux puis les afficher -->
             <v-card v-for="(post,index) in this.$store.state.posts" v-bind:key="index">  <!-- import de post depuis $store.state -->
                 <v-card-title class="v-card-title--color">{{post.title}}</v-card-title>
+                <v-card-title class="v-card-title--date"><em class="date--title">date de creation:</em> {{post.createdAt}}</v-card-title>
                 <v-img class="v-img--post" v-bind:src="post.image" alt="mon image"></v-img>
                 <v-text class="v-text--content"><p>{{post.content}}</p></v-text>
             </v-card>
