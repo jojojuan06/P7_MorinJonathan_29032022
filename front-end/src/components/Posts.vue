8341f63cb509c5c54736f3a03745e44dbdc4ca76
P2-openclassroom <jonh_06@live.fr>
2022-04-17 21:29:10 +0200

    add like and comment icon

--- front-end/src/components/Posts.vue
+++ front-end/src/components/Posts.vue
@@ -10,10 +10,19 @@
                 <v-text class="v-text--content"><p>{{post.content}}</p></v-text>
                 <!-- boucle sur chaque comment du post et l'affiche -->
                 <hr>
-                <v-text v-for="(comment,index) in post.Comments" :key="index"><p>{{ comment.content }}</p></v-text>
+                <div class="avatar_container">
+                <v-avatar>
+                    <v-img class="avatar--img .rounded-lg" v-if="post.User.profile_img == '' "  src="../images/pngtree-vector-avatar-icon-png-image_702436.png"></v-img>
+                    <v-img class="avatar--img .rounded-lg" v-else  v-bind:src="post.User.profile_img"></v-img>
+                </v-avatar>
+                <v-text class="avatar--comment" v-for="(comment,index) in post.Comments" :key="index"><p>{{ comment.content }}</p></v-text>
+                </div>
                 <hr>
-                <v-btn class="btn--like">
-                    <v-icon class="btn--icon">mdi-thumb-up</v-icon>
+                <v-btn v-if="post.likes == 0" class="btn--like">
+                    <v-icon  class="btn--icon">mdi-thumb-up</v-icon>
+                </v-btn>
+                <v-btn v-else class="btn--notLike">
+                    <v-icon  class="btn--icon">mdi-thumb-up</v-icon>
                 </v-btn>
                 <v-badge class="btn--badge" color="info" :content="'+' + post.likes" inline></v-badge>
             </v-card>
@@ -45,11 +54,31 @@ export default {
     min-width: 0px;
     margin-top: 4px;
 }
+.v-btn.btn--notLike {
+    border-radius: 50%;
+    width: 1px;
+    height: 30px;
+    background-color:green;
+    box-shadow: none;
+    min-width: 0px;
+    margin-top: 4px;
+}
 .btn--icon {
     color: white;
 }
 .btn--badge {
     margin-top: 4px;
+}
+.avatar--img {
+    display: flex;
+    height: 20px;
+}
+.avatar_container{
+    padding-top: 2px;
+    display: flex;
+}
+.avatar--comment {
+    margin-left: 16px
 }
 @import url('../style/posts.css');
 </style>
