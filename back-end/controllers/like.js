19982e498ee20c08eacf1ec8367bddb7028b3bda
P2-openclassroom <jonh_06@live.fr>
2022-05-02 16:13:35 +0200

    comment a faire par la suite

--- back-end/controllers/like.js
+++ back-end/controllers/like.js
@@ -2,7 +2,7 @@
 
 //valide et nettoie uniquement les chaînes (validation de l'email)
 const validator = require('validator'); 
-const { Post, Like,User } = require('../models')
+const { Post, Like} = require('../models')
 
 //creation d'un like Post
 exports.createLike = (req, res, next) => { 
@@ -28,27 +28,6 @@ exports.createLike = (req, res, next) => {
             })
         }).catch(error => res.status(500).json({message: `nous faisons face a cette: ${error}` }))
     }    
-
-
-//mettre a jour d'un Like PUT
-exports.updateLike = (req, res, next) => {//exporter une function createuser / contenue de la route post / creation dun post
-    Like.findOne({ where:{ id: req.params.id,}})
-    .then(like => { // si l'utilisateur et admin il peut modif les utili ou juste l'util modif sont profil
-        if (!like) { // si le like n'existe pas
-            return res.status(404).json({ message: "Le like n'existe pas !"})
-        }
-        if (like.userId === req.auth.userId) {
-            let newLike = Object.assign(like,req.body); // remplace le post par le new post (objet,permet d'envoyer des champ vide(recupere un champ)) 
-            newLike.save() //sauvegarde le nouveau post
-            .then(() => res.status(200).json({ message: 'Like modifié !'}))// retourne la response 200 pour ok pour la methode http , renvoi objet modifier
-            .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` }));    
-            } else {
-            res.status(403).json({ message: `vous n'etes pas autoriser a modifiée ce like` });  
-        }
-    })
-    .catch(error => res.status(404).json({ message: `nous faisons face a cette: ${error}` })); 
-};
-//-------------
 
 //supprimer un Like DELETE
 exports.deleteLike = (req, res, next) => {
@@ -87,23 +66,45 @@ exports.deleteLike = (req, res, next) => {
         
     }).catch(error => res.status(500).json({ message: `nous faisons face a cette: ${error}` }));
 };
-    //-------------
+//-------------
     
+//a faire par la suite 
+
+//mettre a jour d'un Like PUT
+//     exports.updateLike = (req, res, next) => {//exporter une function createuser / contenue de la route post / creation dun post
+//     Like.findOne({ where:{ id: req.params.id,}})
+//     .then(like => { // si l'utilisateur et admin il peut modif les utili ou juste l'util modif sont profil
+//         if (!like) { // si le like n'existe pas
+//             return res.status(404).json({ message: "Le like n'existe pas !"})
+//         }
+//         if (like.userId === req.auth.userId) {
+//             let newLike = Object.assign(like,req.body); // remplace le post par le new post (objet,permet d'envoyer des champ vide(recupere un champ)) 
+//             newLike.save() //sauvegarde le nouveau post
+//             .then(() => res.status(200).json({ message: 'Like modifié !'}))// retourne la response 200 pour ok pour la methode http , renvoi objet modifier
+//             .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` }));    
+//             } else {
+//             res.status(403).json({ message: `vous n'etes pas autoriser a modifiée ce like` });  
+//         }
+//     })
+//     .catch(error => res.status(404).json({ message: `nous faisons face a cette: ${error}` })); 
+// };
+//-------------
+
     //recuperer un like GET
-    exports.getOneLike = (req, res, next) => { 
-        let id = req.params.id // avoir acces  dans l'objet req.pams.id
-        Like.findOne( { where:{id: id},//trouver un objet avec where , on pass l'objet en conparaison id  egal le parm de req id
-        }) 
-        .then(like => res.status(200).json(like)) // retourne la response 200 pour ok pour la methode http , renvoi l'objet (un objet)si il existe dans la Bd
-        .catch(error => res.status(404).json({ message: `Like non trouvé: ${error}` }));
-    }
-    //-------------
+    // exports.getOneLike = (req, res, next) => { 
+    //     let id = req.params.id // avoir acces  dans l'objet req.pams.id
+    //     Like.findOne( { where:{id: id},//trouver un objet avec where , on pass l'objet en conparaison id  egal le parm de req id
+    //     }) 
+    //     .then(like => res.status(200).json(like)) // retourne la response 200 pour ok pour la methode http , renvoi l'objet (un objet)si il existe dans la Bd
+    //     .catch(error => res.status(404).json({ message: `Like non trouvé: ${error}` }));
+    // }
+    // //-------------
     
-    //recuperer tous les like GET ALL
-    exports.getAllLike = (req, res, next) => {    
-        //création des objet-----------
-        Like.findAll({include:User}) //recuperer la table user egalement
-            .then(likes => res.status(200).json(likes)) // retourne la response 200 pour ok pour la methode http , revoi le tableaux des users recu
-            .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` })); 
-        }
+    // //recuperer tous les like GET ALL
+    // exports.getAllLike = (req, res, next) => {    
+    //     //création des objet-----------
+    //     Like.findAll({include:User}) //recuperer la table user egalement
+    //         .then(likes => res.status(200).json(likes)) // retourne la response 200 pour ok pour la methode http , revoi le tableaux des users recu
+    //         .catch(error => res.status(400).json({ message: `nous faisons face a cette: ${error}` })); 
+    //     }
     //-------------
