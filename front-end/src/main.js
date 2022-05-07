153f54567ec3fc04083e02ae15baaba2f3d890c3
P2-openclassroom <jonh_06@live.fr>
2022-05-07 21:03:53 +0200

    change comment

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,4 +1,5 @@
 // importe le composant racine App à partir d'un composant à fichier unique.
+//import des librairies
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
